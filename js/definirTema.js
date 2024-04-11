4b911eeaa80edd2995cdd560dd897b354f323873
Franklin Cortés <andres10cortes07@gmail.com>
2024-04-11 10:45:40 -0500

    Mejora en hojas de estilo y cambio de tema usando JS

---
+++ js/definirTema.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function activarPanel() {
+    const contenedor = document.querySelector(".cont-colores");
+    const boton = document.querySelector(".open-panel");
+  
+    contenedor.classList.toggle("active");
+    boton.classList.toggle("active");
+}
+
+const btnOscuro = document.querySelector(".btn-oscuro");
+const btnClaro = document.querySelector(".btn-claro");
+let url = window.location.href;
+let nombreArchivo = url.split('/').pop();
+var link = document.getElementById("fileCss");
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    cambiarTema()
+})
+
+btnOscuro.addEventListener("click", ()=>{
+    localStorage.setItem("tema", "oscuro");
+    cambiarTema()
+})
+
+btnClaro.addEventListener("click", ()=>{
+    localStorage.setItem("tema", "claro");
+    cambiarTema()
+})
+
+const cambiarTema = ()=> {
+    if(localStorage.tema == "claro") link.href = "css/style_sobremi_claro.css";
+    else {
+        link.href = "css/style.css"
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,38 +1,6 @@
 AOS.init();
 emailjs.init('fNonciQZMH90FcxjL');
 
-function activarPanel() {
-  const contenedor = document.querySelector(".cont-colores");
-  const boton = document.querySelector(".open-panel");
-
-  contenedor.classList.toggle("active");
-  boton.classList.toggle("active");
-}
-
-var link = document.getElementById("fileCss");
-
-function principal() {
-  link.href = "css/style.css";
-}
-
-function index_claro() {
-  link.href = "css/style_claro.css";
-}
-
-function sobre_mi_claro() {
-  link.href = "css/style_sobremi_claro.css";
-}
-
-function portafolio_claro() {
-  link.href = "css/style_portafolio_claro.css";
-}
-
-function contacto_claro() {
-  link.href = "css/style_contacto_claro.css";
-}
-
-
-
 // Función para validar el nombre
 function validarNombre() {
   const nombreInput = document.querySelector(".nombre_form");
