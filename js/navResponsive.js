8816273a2c98f3f540718dd22b5898782c48ed74
Franklin Cortés <andres10cortes07@gmail.com>
2024-05-02 11:50:26 -0500

    changing colors in nav and writing animation

--- js/navResponsive.js
+++ js/navResponsive.js
@@ -6,9 +6,9 @@ const navResponsive = () => {
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas offcanvas-end text-bg-dark w-75" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Franklin Cortés</h5>
+          <h5 class="offcanvas-title text-white" id="offcanvasDarkNavbarLabel">Franklin Cortés</h5>
           <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
